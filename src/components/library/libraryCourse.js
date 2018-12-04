878ea4ae6c55436e88e3735d279915b4b57e1cf8
cchelsis <stepforwardinyourlife@gmail.com>
2018-12-04 12:25:43 -0700

    built library course component with grid and library grid

---
+++ src/components/library/libraryCourse.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class LibraryCourse extends Component {
+    render() {
+        return (
+            <div className="library-course">
+                <label className="library_course__title">Problem Solving></label>
+                {/* {icon component} */}
+                {/* {arrow component} */}
+                {/* {action button component} */}
+                <div>
+                    <label>Course Description</label>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur quis ornare nunc, at rutrum elit. Nulla a facilisis enim. Proin volutpat metus massa, vel ullamcorper risus iaculis nec. Sed sem lectus, vulputate nec tincidunt ut, ultricies quis neque. </p>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default LibraryCourse;
