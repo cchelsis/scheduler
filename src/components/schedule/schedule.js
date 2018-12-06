d768e14ac32669ab6bea67b0533630e827785d12
cchelsis <stepforwardinyourlife@gmail.com>
2018-12-06 15:21:42 -0700

    built progress tracker component

--- src/components/schedule/schedule.js
+++ src/components/schedule/schedule.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 
 import Gradient from '../gradient';
-import ScheduleCourse from '../scheduleCourse';
+import ScheduleCourse from './scheduleCourse';
+import ProgressTracker from './progressTracker';
 
 class Schedule extends Component {
     render() {
@@ -11,6 +12,7 @@ class Schedule extends Component {
                 <ScheduleCourse title={'Problem Solving'}/>
                 <ScheduleCourse title={'Problem Solving'}/>
                 <ScheduleCourse title={'Problem Solving'}/>
+                <ProgressTracker/>
                 <Gradient/> 
             </div>
         )
